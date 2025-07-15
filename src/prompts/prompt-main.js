9337ba54c3af96da5ba3bf81dd7ac10483988173
Matheus Araujo <theusnascimento386@gmail.com>
2025-07-14 21:57:09 -0300

    feat: prompt qrcode

---
+++ src/prompts/prompt-main.js
@@ -0,0 +1,13 @@
+import chalk from "chalk"
+
+const mainPrompt = [
+    {
+        name: "select",
+        description: chalk.yellow("Escolha a ferramenta (1 - QRCODE ou (2 - PASSWORD: "),
+        pattern: /^[1-2]+$/,  // O usuário só pode escolher [1 ou 2]
+        message: chalk.red("Escolha apenas entre 1 e 2"),  // Se a pessoa digitar algum número não desejado mando esse error. 
+        require: true
+    }
+]
+
+export default mainPrompt
