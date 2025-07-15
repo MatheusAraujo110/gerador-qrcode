2647e259a2d334431f356fa70dd11496d11c3132
Matheus Araujo <theusnascimento386@gmail.com>
2025-07-14 22:04:33 -0300

    feat: password and grcode generation services

---
+++ src/service/password/handle.js
@@ -0,0 +1,30 @@
+async function permittedCharacters() {
+    let permitted = []
+
+    if (process.env.UPPERCASE_LETTERS === "true") permitted.push(..."ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+    if (process.env.LOWERCASE_LETTERS === "true") permitted.push(..."abcdefghijklmnopqrstuvwxyz")
+
+    if (process.env.NUMBERS === "true") permitted.push(..."0123456789")
+
+    if (process.env.SPECIAL_CHARACTERS === "true") permitted.push(..."!@#$%&*()-_")
+
+    return permitted
+}
+
+async function handle() {
+    let characters = []
+    let password = ""
+
+    const passwordLength = process.env.PASSWORD_LENGTH
+    characters = await permittedCharacters()
+
+    for (let i = 0; i < passwordLength; i++) {
+        const index = Math.floor(Math.random() * characters.length);  // De tudo que tem dentro da characters[], pega e faz um sorteio.
+        password += characters[index]  // Eu vou pegar do meu characters de um index aleatório.
+    }
+
+    return password
+}
+
+export default handle
